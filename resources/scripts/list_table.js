b55219d30830bc742d4c410aa4c4ec3cd332aed6
XMantrixX <kacperekjakubowski222@outlook.com>
2025-11-02 07:11:59 +0100

    tag

--- resources/scripts/list_table.js
+++ resources/scripts/list_table.js
@@ -4,6 +4,7 @@ function create_table(type) {
     var y = 0;
     var z = 0;
     var img;
+    var im;
 
     table.innerHTML = "";
 
@@ -13,13 +14,21 @@ function create_table(type) {
         }
         img = i + "-" + type + ".png";
         cell = row.insertCell(x);
-        cell.innerHTML = "<img src = 'resources/images/all_the_honses/"+ i +"/"+ img +"'id = '"+ i +"' onmouseover = 'while_over("+ i +")' onmouseout = 'while_out("+ i +")'>";
+        im = i+"im";
+        cell.innerHTML = "<div class = 'tag' id = '"+ i +"' onmouseover = 'while_over("+ i +")' onmouseout = 'while_out("+ i +")'></div><img src = 'resources/images/all_the_honses/"+ i +"/"+ img +"'onmouseover = 'while_over("+ i +")' onmouseout = 'while_out("+ i +")' id = '"+ im +"'>";
         z++;
     }
 }
 function while_over(x) {
     var th1s = document.getElementById(x);
+    var th1s_i = document.getElementById(x+"im");
+    th1s.innerHTML = x;
+    th1s.style.display = "block";
+    th1s_i.style.filter = "blur(3px)";
 }
 function while_out(x) {
     var th1s = document.getElementById(x);
+    var th1s_i = document.getElementById(x+"im");
+    th1s.style.display = "none";
+    th1s_i.style.filter = "none";
 }
