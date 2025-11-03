d524725830c120991eaa2b9dbbd57511bd31acdd
XMantrixX <kacperekjakubowski222@outlook.com>
2025-11-04 00:16:13 +0100

    more themes

--- resources/scripts/all_horse_themes.js
+++ resources/scripts/all_horse_themes.js
@@ -1,3 +1,65 @@
+function dark_mode() {
+	set_perm_cookie("theme", "dark");
+	var theme = document.getElementById("theme");
+	theme.href = "resources/styles/list_styles/themes/theme_dark.css"
+}
+
+function light_mode() {
+	set_perm_cookie("theme", "light");
+	var theme = document.getElementById("theme");
+	theme.href = "resources/styles/list_styles/themes/theme_light.css"
+}
+
+function purple_mode() {
+	set_perm_cookie("theme", "purple");
+	var theme = document.getElementById("theme");
+	theme.href = "resources/styles/list_styles/themes/theme_purple.css"
+}
+
+function matik_mode() {
+	set_perm_cookie("theme", "matik");
+	var theme = document.getElementById("theme");
+	theme.href = "resources/styles/list_styles/themes/theme_matik.css"
+}
+
+function still_mode() {
+	set_perm_cookie("theme", "still");
+	var theme = document.getElementById("theme");
+	theme.href = "resources/styles/list_styles/themes/theme_still.css"
+}
+
+function load_theme() {
+    let theme = get_cookies().theme;
+
+    console.log(theme);
+    console.log(get_cookies());
+
+    switch(theme) {
+        case "dark":
+            dark_mode();
+            break;
+        case "light":
+            light_mode();
+            break;
+        case "purple":
+            purple_mode();
+            break;
+        case "matik":
+            matik_mode();
+            break;
+        case "still":
+            still_mode();
+            break;
+
+        default:
+            dark_mode();
+            break;
+    }
+}
+
+
+
+/*
 function getIds(wanted) {
     var back_button = document.getElementById("back");
     var select = document.getElementsByClassName("selection");
@@ -54,6 +116,7 @@ function dark_mode() {
 	    color: #888888;
     `;
 }
+
 
 
 function light_mode() {
@@ -202,32 +265,4 @@ function still_mode() {
 	    color: #b8827dff;
     `;
 }
-
-function load_theme() {
-    let theme = get_cookies().theme;
-
-    console.log(theme);
-    console.log(get_cookies());
-
-    switch(theme) {
-        case "dark":
-            dark_mode();
-            break;
-        case "light":
-            light_mode();
-            break;
-        case "purple":
-            purple_mode();
-            break;
-        case "matik":
-            matik_mode();
-            break;
-        case "still":
-            still_mode();
-            break;
-
-        default:
-            dark_mode();
-            break;
-    }
-}
+*/
